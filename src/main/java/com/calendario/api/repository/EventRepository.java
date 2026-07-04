03787bb6260ddaf34d0fead9c0ec03c268e482df
Ilbert Rudá <ilbertruda0@gmail.com>
2026-07-04 19:22:02 -0300

    Remove unnecessary comment from EventRepository
    Removed comment about custom query methods.

--- src/main/java/com/calendario/api/repository/EventRepository.java
+++ src/main/java/com/calendario/api/repository/EventRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-    // Métodos customizados de query podem ser adicionados aqui se necessário
 }
